988037cd3474d2e449d950d79e8181b793850fc0
zetator-gif <zetator@atomicmail.io>
2026-01-12 08:53:23 +0100

    Update gtk.c

--- 8/C/gtk.c
+++ 8/C/gtk.c
@@ -1,2 +1,8 @@
 #include <gtk/gtk.h>
+// single line header
+static void
 
+activate (GtkApplication *app, gpointer  user_data)
+// GtkApplication is a type
+// *app is a pointer(memory address) to a GtkApplication
+// 
