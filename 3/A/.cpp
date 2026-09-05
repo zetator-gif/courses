217f17db17e21a4c42f4d051e303e154022fcd3f
zetator-gif <zetator@atomicmail.io>
2026-09-05 11:43:42 +0200

    undeclared handler

--- 3/A/.cpp
+++ 3/A/.cpp
@@ -1,21 +1,27 @@
-//https://www.google.com/search?q=populating+set+with+A1+to+H8&sca_esv=c301df9aa710140d&ei=Ft-baoqMBZbni-gPt_S84Q4&biw=1197&bih=1180&ved=2ahUKEwiK75rGjteWAxWW8wIHHTc6L-wQ4dUDegQIBhAM&uact=5&oq=populating+set+with+A1+to+H8&gs_lp=Egxnd3Mtd2l6LXNlcnAiHHBvcHVsYXRpbmcgc2V0IHdpdGggQTEgdG8gSDgyBRAhGKABMgUQIRigATIFECEYoAFIkztQoAhY7jlwAngBkAEAmAGFAaAB4wyqAQQzLjEyuAEDyAEA-AEBmAIRoAKVDcICChAAGEcY1gQYsAPCAgUQABjvBcICCBAAGIAEGKIEwgIEECEYFZgDAIgGAZAGCJIHBDUuMTKgB70esgcEMy4xMrgHjw3CBwYxLjE1LjHIBxuACAE&sclient=gws-wiz-serp#include <bits/stdc++.h>
-using namespaces std;
+//https://www.google.com/search?q=populating+set+with+A1+to+H8&sca_esv=c301df9aa710140d&ei=Ft-baoqMBZbni-gPt_S84Q4&biw=1197&bih=1180&ved=2ahUKEwiK75rGjteWAxWW8wIHHTc6L-wQ4dUDegQIBhAM&uact=5&oq=populating+set+with+A1+to+H8&gs_lp=Egxnd3Mtd2l6LXNlcnAiHHBvcHVsYXRpbmcgc2V0IHdpdGggQTEgdG8gSDgyBRAhGKABMgUQIRigATIFECEYoAFIkztQoAhY7jlwAngBkAEAmAGFAaAB4wyqAQQzLjEyuAEDyAEA-AEBmAIRoAKVDcICChAAGEcY1gQYsAPCAgUQABjvBcICCBAAGIAEGKIEwgIEECEYFZgDAIgGAZAGCJIHBDUuMTKgB70esgcEMy4xMrgHjw3CBwYxLjE1LjHIBxuACAE&sclient=gws-wiz-serp
+
+ #include <iostream>
+#include <set>
+// #include <string>
+
 
 int main()
 {
-    std::set<int> myset;
-    
-int s
-int t 
+
+// int s
+// int t 
 
 // empty set
-set<int> s1;
-
+std::set<int> s1;
+    for (int pos = *A1; pos < *H8; ++pos)
 // common vertex
-my_set = set()
-for letter in "ABCDEFGH":
-    my_set.add(letter)
-for number in range(1, 9):
-    my_set.add(number)
+}
+std::cout << std::endl;
+
+
+// print set
+  for (int value : s1) {
+        std::cout << value << " ";
 return 0;
 }
+// undeclared handler
