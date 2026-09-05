94dcd04c9f23a038f6977899d763ea3d3c3d6cae
zetator-gif <zetator@atomicmail.io>
2026-09-05 17:40:51 +0200

    .

--- CPP/.cpp
+++ CPP/.cpp
@@ -1,1 +1,34 @@
-#bits/stdc++
+std::string text;
+std::getline(std::cin, text);
+
+std::set<std::string> filter;
+
+for(int i = 0; i < text.length(); i++){
+    // filter.insert(text[i]); ::faulty
+    filter.insert(std::string(1, text[i]));
+
+}
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <set>
+
+int main(){
+    
+    std::string text;
+    std::getline(std::cin, text);
+    
+    std::set<std::string> filter;
+    
+    for(int i = 0; i< text.length(); i++){
+        filter.insert(std::string(1, text[i]));
+    }
+    for (int value : text) {
+        
+     std::cout << text << " ";
+     return 0;
+    }
+}
+    
